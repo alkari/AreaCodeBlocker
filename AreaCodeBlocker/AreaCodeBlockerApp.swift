954a93c098db0bca31d89eedd55db98c775cf488
Al Kari <al_kari@manceps.com>
2025-09-18 11:25:25 -0700

    Initial Commit

---
+++ AreaCodeBlocker/AreaCodeBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaCodeBlockerApp.swift
+//  AreaCodeBlocker
+//
+//  Created by Al Kari on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AreaCodeBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
